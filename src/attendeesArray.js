994507890f553b393af229897555499e104ea967
Jack Painter <littlesympathy@jacks-mbp.home>
2019-10-14 01:59:54 +0100

    Remove unnecessary files

--- src/attendeesArray.js
+++
@@ -1,9 +0,0 @@
-import Attendees from "./example_data.json";
-
-const AttendeesArray = () => {
-  return (
-      Object.keys(Attendees).map(key => Attendees[key])
-      );
-};
-
-export default AttendeesArray;
